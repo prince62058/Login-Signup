bfb8fe9820ce1d93be2e594a66d3ddc51249c84b
Prince Kumar <147418512+prince62058@users.noreply.github.com>
2025-06-14 17:13:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let signupbtn = document.querySelector('.signupbtn');
+let signinbtn = document.querySelector('.signinbtn'); 
+let namefield = document.querySelector('.namefield'); 
+let title = document.querySelector('.title');
+let underline = document.querySelector('.underline');
+let text = document.querySelector('.text');
+
+signinbtn.addEventListener('click', () => {
+    namefield.style.maxHeight = "0";
+    title.innerHTML = "Sign In";
+    text.innerHTML = "Forgot Password?";
+    signupbtn.classList.add('disable');
+    signinbtn.classList.remove('disable');
+    underline.style.transform = 'translateX(35px)';
+});
+signupbtn.addEventListener('click', () => {
+    namefield.style.maxHeight = "60px";
+    title.innerHTML = "Sign Up";
+    text.innerHTML = "Password Suggestions";
+    signupbtn.classList.remove('disable');
+    signinbtn.classList.add('disable');
+    underline.style.transform = 'translateX(0)';
+});
